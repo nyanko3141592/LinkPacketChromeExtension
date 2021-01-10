b8fb17ffd672116b12ebbd5ff8e06176ecb0e7d6
nya3_neko2 <takahashinaoki521@gmail.com>
2021-01-11 05:13:19 +0900

    見た目をいいかんじに

--- popup/popup.js
+++ popup/popup.js
@@ -1,6 +1,9 @@
+let nowUrl = "";
+let nowTitle = "";
+let userPacketsUuids = [];
+let packetNames = [];
+
 window.onload = function () {
-    let nowUrl = "";
-    let nowTitle = "";
     chrome.tabs.getSelected(null, function (tab) {
         // 　　現在のサイトの表示
         nowUrl = tab.url;
@@ -9,29 +12,30 @@ window.onload = function () {
         document.getElementById("now-url").innerText = nowUrl;
     });
     //    APIでパケットリストを取得
-    // let userPacketsUuids = ["neko", "dog"];
-    let userPacketsUuids = [];
-    let packetNames = [];
+    userPacketsUuids = ["neko", "dog"];
+    // userPacketsUuids = [];
+    packetNames = [];
     userPacketsUuids.forEach(uuid => {
+        //TODO タイトルに差し替える
         packetNames.push("a");
     })
     //代替のリスト
-    // packetNames = ["ねこ", "いぬ"];
+    packetNames = ["ねこ", "いぬ"];
 
 
     if (userPacketsUuids.length === 0) {
         //　新規リストを作成する(LPに飛ばす)
         $("#submit").remove()
     } else {
-        $("#jumpLP").remove()
+        $("#makeNew").remove()
         //    リストを並べる
         let card = "   <section class=\"card\">\n" +
-            "        <label for=\"@\"></label><input id=\"@\" type=\"checkbox\"/>\n" +
+            "        <label for=\"@\"></label><input id=\"@\" type=\"checkbox\" name=\"checkbox\"/>\n" +
             "        <label for=\"@\">Title</label>\n" +
             "    </section>"
         for (let i = 0; i < userPacketsUuids.length; i++) {
             console.log(packetNames[i]);
-            $("#packets").append(card.replace("@", userPacketsUuids[i]).replace("Title", packetNames[i]));
+            $("#packets").append(card.replaceAll("@", userPacketsUuids[i]).replaceAll("Title", packetNames[i]));
         }
     }
     // jump LP
@@ -47,7 +51,7 @@ window.onload = function () {
 document.getElementById("submit").onclick = function () {
     console.log('Submit');
     //    選択されたリストの取得
-
+    let checked = getChecked()
     //    API叩く
 
     //    API正常に叩けた表示
@@ -61,4 +65,18 @@ function jumpToLP() {
     chrome.tabs.query({'active': true}, function (tabs) {
         chrome.tabs.update(tabs[0].id, {url: "https://linkpacket.web.app/"});
     });
+}
+
+// checked
+function getChecked() {
+    let checked = [];
+    const chechboxs = document.getElementsByName("checkbox");
+
+    for (let i = 0; i < chechboxs.length; i++) {
+        if (chechboxs[i].checked) {
+            checked.push(chechboxs[i].id);
+        }
+    }
+    console.log(checked);
+    return checked
 }
